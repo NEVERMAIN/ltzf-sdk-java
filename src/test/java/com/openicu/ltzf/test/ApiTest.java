d5d3cb08b12ea5c76a37f643414f569124292d55
yunqiji <yunqiji@qq.com>
2024-04-15 19:40:53 +0800

    feat: init project

---
+++ src/test/java/com/openicu/ltzf/test/ApiTest.java
@@ -0,0 +1,12 @@
+package com.openicu.ltzf.test;
+
+/**
+ * @description:
+ * @author: 云奇迹
+ * @date: 2024/4/15
+ */
+public class ApiTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
